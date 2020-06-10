777e0d23fb87572ff809537f44c3742e9949b552
Victor Tsyan <53456492+tsyvic@users.noreply.github.com>
2020-06-10 17:24:42 +1200

    Changes

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client();
 
-cons token = 
+const discord_token = process.env.token;
 
 bot.on('ready', () => {
     console.log('This bot is online!');
 })
+
+bot.login(discord_token);
